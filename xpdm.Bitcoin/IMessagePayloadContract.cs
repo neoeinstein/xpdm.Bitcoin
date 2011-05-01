67d2f5e91eeca5020c0684a3bf0acb5ba9654a2c
Marcus Griep <marcus@griep.us>
2011-05-01 16:52:49 -0400

    Begin constructing Message payload types

---
+++ xpdm.Bitcoin/IMessagePayloadContract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace xpdm.Bitcoin
+{
+    [ContractClassFor(typeof(IMessagePayload))]
+    abstract class IMessagePayloadContract : IMessagePayload
+    {
+        public string Command
+        {
+            get
+            {
+                Contract.Ensures(!string.IsNullOrWhiteSpace(Contract.Result<string>()));
+                Contract.Ensures(Contract.Result<string>().Length <= 12);
+
+                return default(string);
+            }
+        }
+
+        public bool IncludeChecksum
+        {
+            get
+            {
+                return default(bool);
+            }
+        }
+    }
+}
---
+++ xpdm.Bitcoin/MessagePayloadBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xpdm.Bitcoin
+{
+    public abstract class MessagePayloadBase : BitcoinSerializableBase, IMessagePayload
+    {
+        public abstract string Command { get; }
+
+        public virtual bool IncludeChecksum
+        {
+            get { return true; }
+        }
+    }
+}
