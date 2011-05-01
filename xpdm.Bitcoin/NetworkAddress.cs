2aa1f96fb3615ef5dd2fbab19f12eded58aa95ac
Marcus Griep <marcus@griep.us>
2011-04-30 23:46:59 -0400

    Add NetworkAddress implementation

---
+++ xpdm.Bitcoin/NetworkAddress.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Net;
+using System.Net.Sockets;
+
+namespace xpdm.Bitcoin
+{
+    public class NetworkAddress : IBitcoinSerializable
+    {
+        public Services Services { get; private set; }
+        public IPEndPoint Endpoint { get; private set; }
+
+        private const uint BYTESIZE = 26;
+        public uint ByteSize
+        {
+            get { return BYTESIZE; }
+        }
+
+        public NetworkAddress(Services services, IPEndPoint endpoint)
+        {
+            Contract.Requires<ArgumentNullException>(endpoint != null, "endpoint");
+            Contract.Requires<ArgumentException>(endpoint.AddressFamily == AddressFamily.InterNetwork || endpoint.AddressFamily == AddressFamily.InterNetworkV6, "Non-IP Addresses are not supported");
+
+            Services = services;
+            Endpoint = endpoint;
+        }
+
+        public NetworkAddress(byte[] buffer, int offset)
+        {
+            Contract.Requires<ArgumentOutOfRangeException>(offset >= 0, "offset");
+            Contract.Requires<ArgumentOutOfRangeException>(offset <= buffer.Length - BYTESIZE, "offset");
+
+            Services = (Services)buffer.ReadUInt64(offset);
+            var address = new byte[16];
+            Array.Copy(buffer, offset + ADDRESS_IPV6_OFFSET, address, 0, 16);
+            Endpoint = new IPEndPoint(
+                          new IPAddress(address),
+                          buffer.ReadUInt16BE(offset + PORT_OFFSET));
+        }
+
+        private const int ADDRESS_IPV6_OFFSET = 8;
+        private const int ADDRESS_IPV4_OFFSET = ADDRESS_IPV6_OFFSET + 12;
+        private const int PORT_OFFSET = ADDRESS_IPV6_OFFSET + 16;
+
+        [Pure]
+        public void WriteToBitcoinBuffer(byte[] buffer, int offset)
+        {
+            ((ulong)Services).WriteBytes(buffer, offset);
+            switch(Endpoint.AddressFamily)
+            {
+                case AddressFamily.InterNetwork:
+                    ((ushort)0xFFFF).WriteBytes(buffer, ADDRESS_IPV4_OFFSET - 2);
+                    Endpoint.Address.GetAddressBytes().CopyTo(buffer, offset + ADDRESS_IPV4_OFFSET);
+                    break;
+                case AddressFamily.InterNetworkV6:
+                    Endpoint.Address.GetAddressBytes().CopyTo(buffer, offset + ADDRESS_IPV6_OFFSET);
+                    break;
+            }
+            ((ushort)Endpoint.Port).WriteBytesBE(buffer, offset + PORT_OFFSET);
+        }
+
+        [ContractInvariantMethod]
+        private void __Invariant()
+        {
+            Contract.Invariant(Endpoint != null);
+            Contract.Invariant(Endpoint.AddressFamily == AddressFamily.InterNetwork || Endpoint.AddressFamily == AddressFamily.InterNetworkV6, "Non-IP Addresses are not supported");
+        }
+    }
+}
