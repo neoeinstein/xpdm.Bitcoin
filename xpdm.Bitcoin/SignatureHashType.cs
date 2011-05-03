71d11af06abc699ab20ab11d38ce35e70cc2a2c6
Marcus Griep <marcus@griep.us>
2011-05-03 14:51:10 -0400

    Add enums to support building Script capability

---
+++ xpdm.Bitcoin/SignatureHashType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace xpdm.Bitcoin
+{
+    [Flags]
+    public enum SignatureHashType : byte
+    {
+        All = 0x00,
+        None = 0x01,
+        Single = 0x02,
+        OutputMask = 0x1f,
+        AnyoneCanPay = 0x80,
+    }
+}
