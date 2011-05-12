f274bcf94fb6850d3bf407da6a6a834b2d54cd24
Marcus Griep <marcus@griep.us>
2011-05-12 11:00:16 -0400

    Add additional hash functions
    Includes SHA-1, SHA-256, and RIPEMD-160

--- xpdm.Bitcoin/HashUtil.cs
+++ xpdm.Bitcoin/HashUtil.cs
@@ -56,14 +56,78 @@ namespace xpdm.Bitcoin
             Contract.Ensures(Contract.Result<byte[]>() != null);
             Contract.Ensures(Contract.Result<byte[]>().Length == Hash160ByteLength);
 
+            return Ripemd160(Sha256(buffer, offset, length));
+        }
+
+        public static byte[] Sha1(byte[] buffer)
+        {
+            Contract.Requires<ArgumentNullException>(buffer != null, "buffer");
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+            Contract.Ensures(Contract.Result<byte[]>().Length == Hash160ByteLength);
+
+            return Sha1(buffer, 0, buffer.Length);
+        }
+
+        public static byte[] Sha1(byte[] buffer, int offset, int length)
+        {
+            Contract.Requires<ArgumentNullException>(buffer != null, "buffer");
+            Contract.Requires<ArgumentOutOfRangeException>(0 <= offset && offset <= buffer.Length, "offset");
+            Contract.Requires<ArgumentOutOfRangeException>(0 <= length, "length");
+            Contract.Requires<ArgumentOutOfRangeException>(offset + length <= buffer.Length, "length");
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+            Contract.Ensures(Contract.Result<byte[]>().Length == Hash160ByteLength);
+
+            using (var sha = SHA1.Create())
+            {
+                return sha.ComputeHash(buffer, offset, length);
+            }
+        }
+
+        public static byte[] Ripemd160(byte[] buffer)
+        {
+            Contract.Requires<ArgumentNullException>(buffer != null, "buffer");
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+            Contract.Ensures(Contract.Result<byte[]>().Length == Hash160ByteLength);
+
+            return Ripemd160(buffer, 0, buffer.Length);
+        }
+
+        public static byte[] Ripemd160(byte[] buffer, int offset, int length)
+        {
+            Contract.Requires<ArgumentNullException>(buffer != null, "buffer");
+            Contract.Requires<ArgumentOutOfRangeException>(0 <= offset && offset <= buffer.Length, "offset");
+            Contract.Requires<ArgumentOutOfRangeException>(0 <= length, "length");
+            Contract.Requires<ArgumentOutOfRangeException>(offset + length <= buffer.Length, "length");
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+            Contract.Ensures(Contract.Result<byte[]>().Length == Hash160ByteLength);
+
+            using (var ripemd = RIPEMD160.Create())
+            {
+                return ripemd.ComputeHash(buffer, offset, length);
+            }
+        }
+
+        public static byte[] Sha256(byte[] buffer)
+        {
+            Contract.Requires<ArgumentNullException>(buffer != null, "buffer");
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+            Contract.Ensures(Contract.Result<byte[]>().Length == Hash256ByteLength);
+
+            return Sha256(buffer, 0, buffer.Length);
+        }
+
+        public static byte[] Sha256(byte[] buffer, int offset, int length)
+        {
+            Contract.Requires<ArgumentNullException>(buffer != null, "buffer");
+            Contract.Requires<ArgumentOutOfRangeException>(0 <= offset && offset <= buffer.Length, "offset");
+            Contract.Requires<ArgumentOutOfRangeException>(0 <= length, "length");
+            Contract.Requires<ArgumentOutOfRangeException>(offset + length <= buffer.Length, "length");
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+            Contract.Ensures(Contract.Result<byte[]>().Length == Hash256ByteLength);
+
             using (var sha = SHA256.Create())
             {
-                var hashBuffer = sha.ComputeHash(buffer, offset, length);
-                using (var ripemd = RIPEMD160.Create())
-                {
-                    hashBuffer = ripemd.ComputeHash(hashBuffer);
-                    return hashBuffer;
-                }
+                return sha.ComputeHash(buffer, offset, length);
             }
         }
     }
