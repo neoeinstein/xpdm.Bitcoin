a4585945007076d3c844be5877c4e581bef9e394
Marcus Griep <marcus@griep.us>
2011-05-12 16:44:54 -0400

    Add OP_VERIFY operation

---
+++ xpdm.Bitcoin/Scripting/Atoms/OpVerifyAtom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xpdm.Bitcoin.Scripting.Atoms
+{
+    public class OpVerifyAtom : OpAtom
+    {
+        public override int OperandCount
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public override int ResultCount
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        protected bool VerifyOrFail { get; private set; }
+
+        protected override void ExecuteImpl(ExecutionContext context)
+        {
+            if (!VerifyOrFail)
+            {
+                return;
+            }
+            if (ExecutionContext.ToBool(context.ValueStack[0]))
+            {
+                context.ValueStack.Pop();
+            }
+            else
+            {
+                context.HardFailure = true;
+            }
+        }
+
+        public OpVerifyAtom() : this(ScriptOpCode.OP_VERIFY, true) { }
+
+        protected OpVerifyAtom(ScriptOpCode opcode, bool verifyOrFail) : base(opcode)
+        {
+            VerifyOrFail = verifyOrFail;
+        }
+    }
+}
