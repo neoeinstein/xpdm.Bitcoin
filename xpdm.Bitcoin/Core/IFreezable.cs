e6bee9e57464eed5d085150a2a5bb47e76bec537
Marcus Griep <marcus@griep.us>
2011-05-16 21:43:15 -0400

    Add IFreezable interface

---
+++ xpdm.Bitcoin/Core/IFreezable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace xpdm.Bitcoin.Core
+{
+    [ContractClass(typeof(Contracts.IFreezableContract))]
+    public interface IFreezable
+    {
+        bool IsFrozen { get; }
+        void Freeze();
+    }
+
+    namespace Contracts
+    {
+        [ContractClassFor(typeof(IFreezable))]
+        public abstract class IFreezableContract : IFreezable
+        {
+            public bool IsFrozen
+            {
+                get
+                {
+                    return default(bool);
+                }
+            }
+
+            public void Freeze()
+            {
+                Contract.Ensures(IsFrozen == true);
+                Contract.EnsuresOnThrow<Exception>(IsFrozen == true);
+            }
+
+            [ContractInvariantMethod]
+            private void __Invariant()
+            {
+                Contract.Invariant(!Contract.OldValue<bool>(IsFrozen) || Contract.OldValue<bool>(IsFrozen) == IsFrozen);
+            }
+        }
+    }
+}
