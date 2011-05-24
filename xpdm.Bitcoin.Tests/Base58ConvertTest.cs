25102de4d563a75a6561594020d7cfab9713b440
Marcus Griep <marcus@griep.us>
2011-05-24 19:22:21 -0400

    Begin setting up tests for Base58 encoder

---
+++ xpdm.Bitcoin.Tests/Base58ConvertTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Gallio.Framework.Data;
+using MbUnit.Framework;
+using NHamcrest.Core;
+
+namespace xpdm.Bitcoin.Tests
+{
+    [TestFixture]
+    class Base58ConvertTest
+    {
+        [Test]
+        [Factory("ConversionTuples")]
+        public void EncodeWithCheck([Bind(2)] byte[] plaintext, [Bind(0)] string expected)
+        {
+            var enc = Base58Convert.EncodeWithCheck(plaintext);
+            Assert.That(enc, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Factory("ConversionTuples")]
+        public void DecodeWithCheck([Bind(2)] byte[] expected, [Bind(0)] string encoded)
+        {
+            var plaintext = Base58Convert.DecodeWithCheck(encoded);
+            Assert.Over.Pairs(plaintext, expected, (l, r) => Assert.That(l, Is.EqualTo(r)));
+        }
+
+        [Test, MultipleAsserts]
+        [Factory("ConversionTuples")]
+        public void TryDecodeWithCheck([Bind(2)] byte[] expected, [Bind(0)] string encoded)
+        {
+            byte[] plaintext;
+            var success = Base58Convert.DecodeWithCheck(encoded, out plaintext);
+            Assert.That(success, Is.True());
+            Assert.Over.Pairs(plaintext, expected, (l, r) => Assert.That(l, Is.EqualTo(r)));
+        }
+
+        [Test]
+        [Factory("ConversionTuples")]
+        public void Encode([Bind(4)] byte[] plaintext, [Bind(0)] string expected)
+        {
+            var enc = Base58Convert.Encode(plaintext);
+            Assert.That(enc, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Factory("ConversionTuples")]
+        public void Decode([Bind(4)] byte[] expected, [Bind(0)] string encoded)
+        {
+            var plaintext = Base58Convert.Decode(encoded);
+            Assert.Over.Pairs(plaintext, expected, (l, r) => Assert.That(l, Is.EqualTo(r)));
+        }
+
+        public static IEnumerable<IDataItem> ConversionTuples
+        {
+            get
+            {
+                yield return new DataRow("93VYUMzRG9DdbRP72uQXjaWibbQwygnvaCu9DumcqDjGybD864T", "", "0xeffb309e964684b54e6069f146e2cd6dae936b711a7a98df4097156b9fc9b344eb", "0x4f9a4b14", "0xeffb309e964684b54e6069f146e2cd6dae936b711a7a98df4097156b9fc9b344eb4f9a4b14");
+                yield return new DataRow("5JLoTseHAG7neUS8UrkRdDCefvVb3BaawsckyroWERqeEPWQ14N", "", "0x804549dd72054c536dd34adbb4ddf6907f2c600fb83dcbbc95798c19917ad22317", "0x67e4c2ab", "0x804549dd72054c536dd34adbb4ddf6907f2c600fb83dcbbc95798c19917ad2231767e4c2ab");
+                yield return new DataRow("1PCsELyrLCZLZNNbhBdtqo61hwPrKyxYvs", "", "0xf39145d4e53ffa996b70c51c34a71dab5037946d", "0xa9e36a28", "0xf39145d4e53ffa996b70c51c34a71dab5037946da9e36a28");
+            }
+        }
+    }
+}
--- xpdm.Bitcoin/Base58Convert.cs
+++ xpdm.Bitcoin/Base58Convert.cs
@@ -15,7 +15,9 @@ namespace xpdm.Bitcoin
             Contract.Requires<ArgumentNullException>(plaintext != null);
             Contract.Ensures(Contract.Result<string>() != null);
 
-            var workingValue = new BigInteger(plaintext);
+            var plaintextArr = new byte[plaintext.Length + 1];
+            Array.Copy(plaintext, plaintextArr, plaintext.Length);
+            var workingValue = new BigInteger(plaintextArr);
             StringBuilder sb = new StringBuilder();
             while (workingValue.CompareTo(Base) >= 0)
             {
@@ -23,7 +25,7 @@ namespace xpdm.Bitcoin
                 workingValue = BigInteger.DivRem(workingValue, Base, out remainder);
                 sb.Insert(0, Alphabet[(int)remainder]);
             }
-            sb.Insert(0, Alphabet[0]);
+            sb.Insert(0, Alphabet[(int)workingValue]);
 
             foreach (byte b in plaintext)
             {
@@ -35,6 +37,21 @@ namespace xpdm.Bitcoin
             }
 
             return sb.ToString();
+        }
+
+        public static string EncodeWithCheck(byte[] plaintext)
+        {
+            var plaintextArr = (byte[])plaintext.Clone();
+            Array.Reverse(plaintextArr);
+            var hash = Cryptography.CryptoFunctionProviderFactory.Default.Hash256(plaintextArr);
+            var plaintextExtended = new byte[plaintext.Length + 4];
+            Array.Copy(plaintext, plaintextExtended, plaintext.Length);
+            plaintextExtended[3] = hash[0];
+            plaintextExtended[2] = hash[1];
+            plaintextExtended[1] = hash[2];
+            plaintextExtended[0] = hash[3];
+            Array.Copy(hash.Bytes, 0, plaintextExtended, plaintext.Length, 4);
+            return Encode(plaintextExtended);
         }
 
         public static byte[] Decode(string enc)
@@ -50,7 +67,7 @@ namespace xpdm.Bitcoin
         {
             Contract.Requires<ArgumentNullException>(enc != null);
             Contract.Requires(Contract.ForAll(0, enc.Length, i => Alphabet.IndexOf(enc[i]) != -1));
-            Contract.Ensures(Contract.Result<BigInteger>() >= 0);
+            //Contract.Ensures(Contract.Result<BigInteger>().ToByteArray()[Contract.Result<BigInteger>().ToByteArray().Length - 1] != 0);
 
             var workingValue = BigInteger.Zero;
             for (int i = 0; i < enc.Length; ++i)
@@ -58,7 +75,44 @@ namespace xpdm.Bitcoin
                 var index = new BigInteger(Alphabet.IndexOf(enc[i]));
                 workingValue = workingValue + index * BigInteger.Pow(Base, enc.Length - 1 - i);
             }
+            var bytes = workingValue.ToByteArray();
+            if (bytes[bytes.Length - 1] == 0 && workingValue > 0)
+            {
+                var newBytes = new byte[bytes.Length - 1];
+                Array.Copy(bytes, newBytes, newBytes.Length);
+                workingValue = new BigInteger(newBytes);
+            }
             return workingValue;
+        }
+
+        public static byte[] DecodeWithCheck(string enc)
+        {
+            byte[] plaintext;
+            return DecodeWithCheck(enc, out plaintext) ? plaintext : null;
+        }
+
+        public static bool DecodeWithCheck(string enc, out byte[] plaintext)
+        {
+            var plaintextExtended = Decode(enc);
+            var hash = Cryptography.CryptoFunctionProviderFactory.Default.Hash256(plaintextExtended, 0, plaintextExtended.Length - 4).Bytes;
+            if (hash[0] == plaintextExtended[plaintextExtended.Length - 4]
+             && hash[1] == plaintextExtended[plaintextExtended.Length - 3]
+             && hash[2] == plaintextExtended[plaintextExtended.Length - 2]
+             && hash[3] == plaintextExtended[plaintextExtended.Length - 1])
+            {
+                var pt = new byte[plaintextExtended.Length - 4];
+                Array.Copy(plaintextExtended, pt, pt.Length);
+                plaintext = pt;
+                return true;
+            }
+            else
+            {
+                var pt = new byte[plaintextExtended.Length - 4];
+                Array.Copy(plaintextExtended, pt, pt.Length);
+                plaintext = pt;
+                //plaintext = new byte[0];
+                return false;
+            }
         }
     }
 }
