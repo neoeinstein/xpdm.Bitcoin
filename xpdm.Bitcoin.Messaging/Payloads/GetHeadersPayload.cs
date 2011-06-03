91574010dc059365486a8994f5a5f9bb40ccdbdd
Marcus Griep <marcus@griep.us>
2011-06-02 22:01:08 -0400

    Convert GetHeadersPayload to BitcoinSerializable

--- xpdm.Bitcoin.Messaging/Payloads/GetHeadersPayload.cs
+++ xpdm.Bitcoin.Messaging/Payloads/GetHeadersPayload.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using xpdm.Bitcoin.Core;
 
 namespace xpdm.Bitcoin.Messaging.Payloads
 {
@@ -11,60 +14,44 @@ namespace xpdm.Bitcoin.Messaging.Payloads
         }
 
         public uint Version { get; private set; }
-        public VarInt StartCount { get; private set; }
-        public VarArray<Hash> HashStart { get; private set; }
-        public Hash HashStop { get; private set; }
+        public Hash256[] HashStart { get; private set; }
+        public Hash256 HashStop { get; private set; }
 
-        public GetHeadersPayload(uint version, Hash[] hashStart, Hash hashStop)
+        public GetHeadersPayload(uint version, Hash256[] hashStart, Hash256 hashStop)
         {
             Contract.Requires<ArgumentNullException>(hashStart != null, "hashStart");
             Contract.Requires<ArgumentNullException>(hashStop != null, "hashStop");
 
             Version = version;
-            HashStart = new VarArray<Hash>(hashStart);
-            StartCount = new VarInt((uint)hashStart.Length);
+            HashStart = hashStart;
             HashStop = hashStop;
-
-            ByteSize = Version.ByteSize() + StartCount.ByteSize + HashStart.ByteSize + HashStop.ByteSize;
         }
 
-        public GetHeadersPayload(byte[] buffer, int offset)
-            : base(buffer, offset)
+        public GetHeadersPayload(Stream stream) : base(stream) { }
+        public GetHeadersPayload(byte[] buffer, int offset) : base(buffer, offset) { }
+
+        protected override void Deserialize(Stream stream)
         {
-            Contract.Requires<ArgumentNullException>(buffer != null, "buffer");
-            Contract.Requires<ArgumentException>(buffer.Length >= GetHeadersPayload.MinimumByteSize, "buffer");
-            Contract.Requires<ArgumentOutOfRangeException>(offset >= 0, "offset");
-            Contract.Requires<ArgumentOutOfRangeException>(offset <= buffer.Length - GetHeadersPayload.MinimumByteSize, "offset");
-
-            Version = buffer.ReadUInt32(offset);
-            StartCount = new VarInt(buffer, StartCount_Offset(ref offset));
-            HashStart = new VarArray<Hash>(buffer, HashStart_Offset(ref offset));
-            HashStop = new Hash(buffer, HashStop_Offset(ref offset));
-
-            ByteSize = Version.ByteSize() + StartCount.ByteSize + HashStart.ByteSize + HashStop.ByteSize;
+            Version = ReadUInt32(stream);
+            HashStart = ReadVarArray<Hash256>(stream);
+            HashStop = new Hash256(stream);
         }
 
-        private int StartCount_Offset(ref int offset) { return offset += (int)Version.ByteSize(); }
-        private int HashStart_Offset(ref int offset) { return offset += (int)StartCount.ByteSize; }
-        private int HashStop_Offset(ref int offset) { return offset += (int)HashStart.ByteSize; }
-
-        [Pure]
-        public override void WriteToBitcoinBuffer(byte[] buffer, int offset)
+        public override void Serialize(Stream stream)
         {
-            Version.WriteBytes(buffer, offset);
-            StartCount.WriteToBitcoinBuffer(buffer, StartCount_Offset(ref offset));
-            HashStart.WriteToBitcoinBuffer(buffer, HashStart_Offset(ref offset));
-            HashStop.WriteToBitcoinBuffer(buffer, HashStop_Offset(ref offset));
+            Write(stream, Version);
+            WriteVarArray(stream, HashStart);
+            Write(stream, HashStop);
+        }
+
+        public override int SerializedByteSize
+        {
+            get { return BufferOperations.UINT32_SIZE + VarIntByteSize(HashStart.Length) + HashStart.Sum(h => h.SerializedByteSize) + HashStop.SerializedByteSize; }
         }
 
         public static string CommandText
         {
             get { return "getheaders"; }
-        }
-
-        public static int MinimumByteSize
-        {
-            get { return BufferOperations.UINT32_SIZE + VarArray<Hash>.MinimumByteSize + Hash.ConstantByteSize; }
         }
     }
 }
