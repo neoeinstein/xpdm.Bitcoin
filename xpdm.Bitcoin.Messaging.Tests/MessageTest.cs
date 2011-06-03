125f33ecb22dbd563f4f4a3b5b9aa489e619ff3f
Marcus Griep <marcus@griep.us>
2011-06-03 15:59:59 -0400

    Add additional round trip message test

--- xpdm.Bitcoin.Messaging.Tests/MessageTest.cs
+++ xpdm.Bitcoin.Messaging.Tests/MessageTest.cs
@@ -10,7 +10,7 @@ namespace xpdm.Bitcoin.Messaging.Tests
     {
         [Test]
         [Factory("SerializedMessages")]
-        public void Test(byte[] message)
+        public void RoundTripDeserialize(byte[] message)
         {
             var msg = new Message(message, 0);
             Assert.IsNotNull(msg);
@@ -27,8 +27,20 @@ namespace xpdm.Bitcoin.Messaging.Tests
             {
                 yield return Access.GetSerializedData("MsgBlock124009.bin");
                 yield return Access.GetSerializedData("MsgBlock124010.bin");
+
                 var ver = new Payloads.VersionPayload(30000, Services.Node_Network, (Core.Timestamp)System.DateTime.Now, new NetworkAddress(Services.Node_Network, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 6321)), new NetworkAddress(Services.Node_Network, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.0.1"), 5)), 12345678, System.Text.Encoding.ASCII.GetBytes("Test"), 127001);
                 var msg = new Message(Network.Main, ver);
+                yield return msg.SerializeToByteArray();
+
+                var inv = new Payloads.InvPayload(new[]
+                {
+                    new InventoryVector(InventoryObjectType.Msg_Block, Core.Hash256.Empty),
+                    new InventoryVector(InventoryObjectType.Msg_Tx, Core.Hash256.Parse("a1bb3b094946d7fe01776a87334ec8b9099e1853c25ba15c5be1eabf4070274d")),
+                    new InventoryVector(InventoryObjectType.Msg_Block, Core.Hash256.Parse("000000000000b2ecc741843890bd619af991fa1e4c555636867cc759e1265d95")),
+                    new InventoryVector(InventoryObjectType.Msg_Tx, Core.Hash256.Parse("60742d2df2de74fe4d2a2cf08c62e3e61665aa68834ab3a094e591ed1b5b98a8")),
+                    new InventoryVector(InventoryObjectType.Msg_Block, Core.Hash256.Parse("000000000000834dc3f04e4f8ba6433f5a9dd1bffd231e14c9947b36243053ef")),
+                });
+                msg = new Message(Network.Main, inv);
                 yield return msg.SerializeToByteArray();
             }
         }
