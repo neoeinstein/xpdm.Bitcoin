095eecc3e318a2051b572154f479de22b3d6a852
Marcus Griep <marcus@griep.us>
2011-06-03 14:53:44 -0400

    Add test project for Messaging
    Also hooks in to previously defined formatters and converters
    to obtain similar benefits when reuse is required without needing
    to copy and maintain a duplicate in the second assembly.

---
+++ xpdm.Bitcoin.Messaging.Tests/AssemblyFixture.cs
@@ -0,0 +1,41 @@
+﻿using Gallio.Runtime;
+using Gallio.Runtime.Conversions;
+using Gallio.Runtime.Extensibility;
+using Gallio.Runtime.Formatting;
+using MbUnit.Framework;
+using xpdm.Bitcoin.Core;
+using xpdm.Bitcoin.Tests.Converters;
+using xpdm.Bitcoin.Tests.Converters.Core;
+using xpdm.Bitcoin.Tests.Formatters;
+
+namespace xpdm.Bitcoin.Messaging.Tests
+{
+    /// <summary>
+    /// Promote reuse of core converters and formatters by re-registering here.
+    /// </summary>
+    [AssemblyFixture]
+    class AssemblyFixture
+    {
+        private static CustomFormatters formatters = new CustomFormatters();
+        private static CustomConverters converters = new CustomConverters();
+        private static IExtensionPoints extensionPoints;
+
+        [FixtureSetUp]
+        public static void FixtureSetUp()
+        {
+            extensionPoints = (IExtensionPoints)RuntimeAccessor.ServiceLocator.ResolveByComponentId("Gallio.ExtensionPoints");
+
+            extensionPoints.CustomFormatters.Register<byte[]>(ByteArrayFormatter.FormatByteArray);
+            extensionPoints.CustomConverters.Register<string, byte[]>(StringToByteArrayConverter.ConvertStringToByteArray);
+            extensionPoints.CustomConverters.Register<string, Script>(StringToScriptConverter.ConvertStringToScript);
+        }
+
+        [FixtureTearDown]
+        public static void FixtureTearDown()
+        {
+            extensionPoints.CustomFormatters.Unregister<byte[]>();
+            extensionPoints.CustomConverters.Unregister<string, byte[]>();
+            extensionPoints.CustomConverters.Unregister<string, Script>();
+        }
+    }
+}
---
+++ xpdm.Bitcoin.Messaging.Tests/MessageTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Gallio.Framework;
+using MbUnit.Framework;
+using xpdm.Bitcoin.Messaging.Tests.SerializedData;
+
+namespace xpdm.Bitcoin.Messaging.Tests
+{
+    [TestFixture]
+    public class MessageTest
+    {
+        [Test]
+        [Factory("SerializedMessages")]
+        public void Test(byte[] message)
+        {
+            var msg = new Message(message, 0);
+            Assert.IsNotNull(msg);
+            TestLog.WriteLine("Network: {0}", msg.Network);
+            TestLog.WriteLine("Command: {0}", msg.Command);
+            TestLog.WriteLine("PayloadLength: {0}", msg.PayloadLength);
+            TestLog.WriteLine("PayloadPayload: {0}", msg.Payload);
+            var ms = new MemoryStream(msg.SerializedByteSize);
+            msg.Serialize(ms);
+            var bytes = ms.ToArray();
+            Assert.AreElementsEqual(message, bytes);
+        }
+
+        public static IEnumerable<byte[]> SerializedMessages
+        {
+            get
+            {
+                yield return Access.GetSerializedData("MsgBlock124009.bin");
+                yield return Access.GetSerializedData("MsgBlock124010.bin");
+            }
+        }
+    }
+}
