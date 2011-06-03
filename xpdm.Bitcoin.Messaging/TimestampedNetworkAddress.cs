a2d48c2368d6b35ed03a22f26791e45642aee236
Marcus Griep <marcus@griep.us>
2011-06-02 23:14:24 -0400

    Convert TimestampedNetworkAddress to BitcoinSerializable

--- xpdm.Bitcoin.Messaging/TimestampedNetworkAddress.cs
+++ xpdm.Bitcoin.Messaging/TimestampedNetworkAddress.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
+using System.IO;
+using xpdm.Bitcoin.Core;
 
 namespace xpdm.Bitcoin.Messaging
 {
-    public class TimestampedNetworkAddress : SerializableMessageBase
+    public class TimestampedNetworkAddress : BitcoinSerializable
     {
         public uint Timestamp { get; private set; }
         public NetworkAddress Address { get; private set; }
@@ -14,39 +16,26 @@ namespace xpdm.Bitcoin.Messaging
 
             Timestamp = timestamp;
             Address = address;
-
-            ByteSize = (uint)TimestampedNetworkAddress.ConstantByteSize;
         }
 
-        public TimestampedNetworkAddress(byte[] buffer, int offset)
-            : base(buffer, offset)
+        public TimestampedNetworkAddress(Stream stream) : base(stream) { }
+        public TimestampedNetworkAddress(byte[] buffer, int offset) : base(buffer, offset) { }
+
+        protected override void Deserialize(Stream stream)
         {
-            Contract.Requires<ArgumentNullException>(buffer != null, "buffer");
-            Contract.Requires<ArgumentException>(buffer.Length >= TimestampedNetworkAddress.ConstantByteSize, "buffer");
-            Contract.Requires<ArgumentOutOfRangeException>(offset >= 0, "offset");
-            Contract.Requires<ArgumentOutOfRangeException>(offset <= buffer.Length - TimestampedNetworkAddress.ConstantByteSize, "offset");
-
-            Timestamp = buffer.ReadUInt32(offset);
-            Address = new NetworkAddress(buffer, offset + NETADDR_OFFSET);
-
-            ByteSize = (uint)TimestampedNetworkAddress.ConstantByteSize;
+            Timestamp = (Timestamp)ReadUInt32(stream);
+            Address = new NetworkAddress(stream);
         }
 
-        private const int NETADDR_OFFSET = BufferOperations.UINT32_SIZE;
-
-        [Pure]
-        public override void WriteToBitcoinBuffer(byte[] buffer, int offset)
+        public override void Serialize(Stream stream)
         {
-            Timestamp.WriteBytes(buffer, offset);
-            Address.WriteToBitcoinBuffer(buffer, offset + NETADDR_OFFSET);
+            Write(stream, Timestamp);
+            Write(stream, Address);
         }
 
-        public static int ConstantByteSize
+        public override int SerializedByteSize
         {
-            get
-            {
-                return BufferOperations.UINT32_SIZE + NetworkAddress.ConstantByteSize;
-            }
+            get { return BufferOperations.UINT32_SIZE + Address.SerializedByteSize; }
         }
 
         [ContractInvariantMethod]
