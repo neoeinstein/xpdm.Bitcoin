f888105da1bb2135ae5775e99aeb5a42dcf5b421
Marcus Griep <marcus@griep.us>
2011-06-03 15:26:05 -0400

    Add Version payload round trip test

--- xpdm.Bitcoin.Messaging.Tests/MessageTest.cs
+++ xpdm.Bitcoin.Messaging.Tests/MessageTest.cs
@@ -27,6 +27,9 @@ namespace xpdm.Bitcoin.Messaging.Tests
             {
                 yield return Access.GetSerializedData("MsgBlock124009.bin");
                 yield return Access.GetSerializedData("MsgBlock124010.bin");
+                var ver = new Payloads.VersionPayload(30000, Services.Node_Network, (Core.Timestamp)System.DateTime.Now, new NetworkAddress(Services.Node_Network, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 6321)), new NetworkAddress(Services.Node_Network, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.0.1"), 5)), 12345678, System.Text.Encoding.ASCII.GetBytes("Test"), 127001);
+                var msg = new Message(Network.Main, ver);
+                yield return msg.SerializeToByteArray();
             }
         }
     }
