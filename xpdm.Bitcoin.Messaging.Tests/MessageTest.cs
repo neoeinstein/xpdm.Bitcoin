858e03fb4a62bc91f77a6c184d5dd06d42b53863
Marcus Griep <marcus@griep.us>
2011-06-03 23:34:39 -0400

    Add headers message test

--- xpdm.Bitcoin.Messaging.Tests/MessageTest.cs
+++ xpdm.Bitcoin.Messaging.Tests/MessageTest.cs
@@ -28,11 +28,14 @@ namespace xpdm.Bitcoin.Messaging.Tests
                 yield return Access.GetSerializedData("MsgBlock124009.bin");
                 yield return Access.GetSerializedData("MsgBlock124010.bin");
 
-                var ver = new Payloads.VersionPayload(30000, Services.Node_Network, (Core.Timestamp)System.DateTime.Now, new NetworkAddress(Services.Node_Network, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 6321)), new NetworkAddress(Services.Node_Network, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.0.1"), 5)), 12345678, System.Text.Encoding.ASCII.GetBytes("Test"), 127001);
-                var msg = new Message(Network.Main, ver);
-                yield return msg.SerializeToByteArray();
+                Payloads.IPayload payload;
+                Message message;
 
-                var inv = new Payloads.InvPayload(new[]
+                payload = new Payloads.VersionPayload(30000, Services.Node_Network, (Core.Timestamp)System.DateTime.Now, new NetworkAddress(Services.Node_Network, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 6321)), new NetworkAddress(Services.Node_Network, new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.0.1"), 5)), 12345678, System.Text.Encoding.ASCII.GetBytes("Test"), 127001);
+                message = new Message(Network.Main, payload);
+                yield return message.SerializeToByteArray();
+
+                payload = new Payloads.InvPayload(new[]
                 {
                     new InventoryVector(InventoryObjectType.Msg_Block, Core.Hash256.Empty),
                     new InventoryVector(InventoryObjectType.Msg_Tx, Core.Hash256.Parse("a1bb3b094946d7fe01776a87334ec8b9099e1853c25ba15c5be1eabf4070274d")),
@@ -40,8 +43,15 @@ namespace xpdm.Bitcoin.Messaging.Tests
                     new InventoryVector(InventoryObjectType.Msg_Tx, Core.Hash256.Parse("60742d2df2de74fe4d2a2cf08c62e3e61665aa68834ab3a094e591ed1b5b98a8")),
                     new InventoryVector(InventoryObjectType.Msg_Block, Core.Hash256.Parse("000000000000834dc3f04e4f8ba6433f5a9dd1bffd231e14c9947b36243053ef")),
                 });
-                msg = new Message(Network.Main, inv);
-                yield return msg.SerializeToByteArray();
+                message = new Message(Network.Main, payload);
+                yield return message.SerializeToByteArray();
+
+                var b1 = new Core.Block(Access.GetSerializedData("MsgBlock124009.bin"), 24);
+                var b2 = new Core.Block(Access.GetSerializedData("MsgBlock124010.bin"), 24);
+
+                payload = new Payloads.HeadersPayload(new[] { b1, b2, });
+                message = new Message(Network.Main, payload);
+                yield return message.SerializeToByteArray();
             }
         }
     }
